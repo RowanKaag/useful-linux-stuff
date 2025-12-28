244890361d14e76413adb867f9a7a9526b481fbf
Rowan Kaag <github@rowankaag.nl>
2025-12-29 00:09:07 +0100

    Add installation script for Nginx on Ubuntu 24.04

---
+++ nginx/install-on-ubuntu_24.04.sh
@@ -0,0 +1,10 @@
+# https://nginx.org/en/linux_packages.html#Ubuntu
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
+sudo apt update -y
+sudo apt install nginx -y
+sudo service nginx start
